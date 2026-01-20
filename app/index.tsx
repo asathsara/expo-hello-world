c2932620bde2ab1340b3462244f61a2b8f6bc03e
A S <65203960+asathsara@users.noreply.github.com>
2026-01-20 22:27:36 +0530

    feat: implement basic todo list with state management and flatlist

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,158 @@
-import { Text, View } from "react-native";
+import React, { useState } from "react";
+import {
+  FlatList,
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 export default function Index() {
+  const [task, setTask] = useState<string>("");
+  const [todoList, setTodoList] = useState<string[]>([]);
+
+  // Function to add a new task
+  const handleAddTask = () => {
+    if (task.trim().length === 0) return; // Don't add empty tasks
+    setTodoList([...todoList, task]);
+    setTask(""); // Clear input
+    Keyboard.dismiss(); // Hide keyboard
+  };
+
+  // Function to delete a task
+  const completeTask = (index: number) => {
+    let itemsCopy = [...todoList];
+    itemsCopy.splice(index, 1);
+    setTodoList(itemsCopy);
+  };
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Hello World</Text>
+    <View style={styles.container}>
+      {/* 1. Header Area */}
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>Today's tasks</Text>
+
+        <View style={styles.items}>
+          {/* This is where the tasks will go! */}
+          <FlatList
+            data={todoList}
+            renderItem={({ item, index }) => (
+              <TouchableOpacity key={index} onPress={() => completeTask(index)}>
+                <View style={styles.item}>
+                  <View style={styles.itemLeft}>
+                    <View style={styles.square}></View>
+                    <Text style={styles.itemText}>{item}</Text>
+                  </View>
+                  <View style={styles.circular}></View>
+                </View>
+              </TouchableOpacity>
+            )}
+            keyExtractor={(item, index) => index.toString()}
+            contentContainerStyle={{ paddingBottom: 100 }}
+          />
+        </View>
+      </View>
+
+      {/* 2. Write a task wrapper */}
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.writeTaskWrapper}
+      >
+        <TextInput
+          style={styles.input}
+          placeholder={"Write a task"}
+          value={task}
+          onChangeText={(text) => setTask(text)}
+        />
+        <TouchableOpacity onPress={() => handleAddTask()}>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#E8EAED",
+  },
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 30,
+  },
+  items: {
+    marginTop: 10,
+  },
+  writeTaskWrapper: {
+    position: "absolute",
+    bottom: 60,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: "#FFF",
+    borderRadius: 60,
+    borderColor: "#C0C0C0",
+    borderWidth: 1,
+    width: 250,
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: "#FFF",
+    borderRadius: 60,
+    justifyContent: "center",
+    alignItems: "center",
+    borderColor: "#C0C0C0",
+    borderWidth: 1,
+  },
+  addText: {},
+  item: {
+    backgroundColor: "#FFF",
+    padding: 15,
+    borderRadius: 10,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginBottom: 20,
+  },
+  itemLeft: {
+    flexDirection: "row",
+    alignItems: "center",
+    flexWrap: "wrap",
+  },
+  square: {
+    width: 24,
+    height: 24,
+    backgroundColor: "#55BCF6",
+    opacity: 0.4,
+    borderRadius: 5,
+    marginRight: 15,
+  },
+  itemText: {
+    maxWidth: "80%",
+  },
+  circular: {
+    width: 12,
+    height: 12,
+    borderColor: "#55BCF6",
+    borderWidth: 2,
+    borderRadius: 5,
+  },
+});
