6cd06c96d43359484227c378acd1e9ffe2d65ae6
A S <65203960+asathsara@users.noreply.github.com>
2026-01-20 22:31:44 +0530

    refactor: extract Task component and modularize styles

--- app/index.tsx
+++ app/index.tsx
@@ -10,6 +10,7 @@ import {
   TouchableOpacity,
   View,
 } from "react-native";
+import Task from "../components/Task";
 
 export default function Index() {
   const [task, setTask] = useState<string>("");
@@ -41,15 +42,10 @@ export default function Index() {
           <FlatList
             data={todoList}
             renderItem={({ item, index }) => (
-              <TouchableOpacity key={index} onPress={() => completeTask(index)}>
-                <View style={styles.item}>
-                  <View style={styles.itemLeft}>
-                    <View style={styles.square}></View>
-                    <Text style={styles.itemText}>{item}</Text>
-                  </View>
-                  <View style={styles.circular}></View>
-                </View>
-              </TouchableOpacity>
+              <Task
+                text={item}
+                onDelete={() => completeTask(index)}
+              />
             )}
             keyExtractor={(item, index) => index.toString()}
             contentContainerStyle={{ paddingBottom: 100 }}
@@ -81,7 +77,7 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#E8EAED",
+    backgroundColor: "#cee7ffff",
   },
   tasksWrapper: {
     paddingTop: 80,
@@ -106,7 +102,7 @@ const styles = StyleSheet.create({
   input: {
     paddingVertical: 15,
     paddingHorizontal: 15,
-    backgroundColor: "#FFF",
+    backgroundColor: "#ffffffff",
     borderRadius: 60,
     borderColor: "#C0C0C0",
     borderWidth: 1,
@@ -123,36 +119,4 @@ const styles = StyleSheet.create({
     borderWidth: 1,
   },
   addText: {},
-  item: {
-    backgroundColor: "#FFF",
-    padding: 15,
-    borderRadius: 10,
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    marginBottom: 20,
-  },
-  itemLeft: {
-    flexDirection: "row",
-    alignItems: "center",
-    flexWrap: "wrap",
-  },
-  square: {
-    width: 24,
-    height: 24,
-    backgroundColor: "#55BCF6",
-    opacity: 0.4,
-    borderRadius: 5,
-    marginRight: 15,
-  },
-  itemText: {
-    maxWidth: "80%",
-  },
-  circular: {
-    width: 12,
-    height: 12,
-    borderColor: "#55BCF6",
-    borderWidth: 2,
-    borderRadius: 5,
-  },
 });
